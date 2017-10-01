c50b71ab9c75867a311a9bf5fb2f864d07c4f59b
Rebecca Alpert <rebeccaalpert@Rebeccas-MacBook-Air.local>
2017-10-01 11:37:16 -0400

    Built out basic app landing screen (without image)

--- app/src/main/java/org/medfordhistorical/overthemystic/MainActivity.java
+++ app/src/main/java/org/medfordhistorical/overthemystic/MainActivity.java
@@ -1,7 +1,12 @@
 package org.medfordhistorical.overthemystic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps one of the homescreen buttons */
+    public void launchTour(View view) {
+        // Do something in response to button
+        switch (view.getId()) {
+            case R.id.bike:
+                Log.d("MainActivity", "Bike");
+                break;
+            case R.id.walk:
+                Log.d("MainActivity", "Walk");
+                break;
+        }
+    }
+    
 }
