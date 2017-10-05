43cc6f9840a2f82e32ef6240036960fc8ec06b06
Rebecca Alpert <rebeccaalpert@wr-130-64-94-65.medford.tufts.edu>
2017-10-05 10:13:30 -0400

    Changed to two-activity app with intent data

--- app/src/main/java/org/medfordhistorical/overthemystic/MainActivity.java
+++ app/src/main/java/org/medfordhistorical/overthemystic/MainActivity.java
@@ -6,9 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
 public class MainActivity extends AppCompatActivity {
+    public static final String tourType = "org.medfordhistorical.overthemystic.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +16,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /** Called when the user taps one of the home screen buttons */
-    public void launchTour(View view) {
+    public void launchTourSelection(View view) {
+        Intent intent = new Intent(this, StartTourActivity.class);
         // Do something in response to button
         switch (view.getId()) {
             case R.id.bike:
                 // call bike-specific function
-                Intent bikeIntent = new Intent(this, DisplayBikeTourActivity.class);
-                startActivity(bikeIntent);
+                intent.putExtra("tourType", "bike");
+                startActivity(intent);
                 break;
             case R.id.walk:
                 // call walking-specific function
-                Intent walkingIntent = new Intent(this, DisplayWalkingTourActivity.class);
-                startActivity(walkingIntent);
+                intent.putExtra("tourType", "walking");
+                startActivity(intent);
                 break;
         }
     }
