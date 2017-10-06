1d9aa77b73564e73e2a2982152b04d232fc7a9fb
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2017-10-06 02:01:57 -0400

    added markers to map

--- app/src/main/java/org/medfordhistorical/overthemystic/MapActivity.java
+++ app/src/main/java/org/medfordhistorical/overthemystic/MapActivity.java
@@ -1,10 +1,17 @@
 package org.medfordhistorical.overthemystic;
 
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -18,6 +25,30 @@ public class MapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_map);
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+
+        //adds markers to map
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+
+                Pair<Double, Double>[] locations = new Pair[] {
+                        new Pair(42.40742, -71.12012),
+                        new Pair(42.40678, -71.11652),
+                        new Pair(42.40817, -71.11375),
+                        new Pair(42.40909, -71.1154),
+                        new Pair(42.41235, -71.11163),
+                        new Pair(42.41312, -71.11096),
+                        new Pair(42.41679, -71.11527)
+                };
+
+                for (int i = 0; i < locations.length; i++) {
+                    mapboxMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(locations[i].first, locations[i].second))
+                            .title("Tufts University")
+                            .snippet("Welcome jumbos!"));
+                }
+            }
+        });
     }
 
     @Override
