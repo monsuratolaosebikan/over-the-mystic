651a6e679fc16401e0cda66228abcd21bf45f193
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2017-10-09 23:50:23 -0400

    adds polylines to markers showing route

--- app/src/main/java/org/medfordhistorical/overthemystic/MapActivity.java
+++ app/src/main/java/org/medfordhistorical/overthemystic/MapActivity.java
@@ -1,5 +1,6 @@
 package org.medfordhistorical.overthemystic;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -7,18 +8,30 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
+import android.util.Log;
 
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.annotations.PolylineOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.geometry.LatLngBounds;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.services.api.directions.v5.DirectionsCriteria;
+import com.mapbox.services.api.directions.v5.MapboxDirections;
+import com.mapbox.services.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.services.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.services.commons.geojson.LineString;
+import com.mapbox.services.commons.models.Position;
 
 import java.util.List;
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.mapbox.services.Constants.PRECISION_6;
 
 
 public class MapActivity extends AppCompatActivity {
@@ -27,11 +40,13 @@ public class MapActivity extends AppCompatActivity {
     private MapView mapView;
     private RecyclerView recyclerView;
     private List<Site> sites;
+    private String ACCESS_TOKEN = "pk.eyJ1IjoibWVkZm9yZGhpc3RvcmljYWwiLCJhIjoiY2o4ZXNiNHN2M" +
+            "TZycjMzb2ttcWp0dDJ1aiJ9.zt52s3jkwqtDc1I2Fv5cJg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this, "pk.eyJ1IjoibWVkZm9yZGhpc3RvcmljYWwiLCJhIjoiY2o4ZXNiNHN2MTZycjMzb2ttcWp0dDJ1aiJ9.zt52s3jkwqtDc1I2Fv5cJg");
+        Mapbox.getInstance(this, ACCESS_TOKEN);
         setContentView(R.layout.activity_map);
 
         recyclerView = (RecyclerView) findViewById(R.id.rvSite);
@@ -132,6 +147,70 @@ public class MapActivity extends AppCompatActivity {
                       .position(locations[i])
                       .title("Tufts University")
                       .snippet("Welcome jumbos!"));
+
+            getRoute(locations, "bike");
         }
     }
+
+    public void getRoute(LatLng[] locations, String method) {
+
+        String profile;
+
+        if (method.toLowerCase().equals("bike")) profile = DirectionsCriteria.PROFILE_WALKING;
+        else  profile = DirectionsCriteria.PROFILE_CYCLING;
+
+
+        List<Position> coordinates = new ArrayList<>();
+        for (int i = 0; i < locations.length; i++) {
+            Position coordinate = Position.fromCoordinates(locations[i].getLongitude(), locations[i].getLatitude());
+            coordinates.add(coordinate);
+        }
+
+        MapboxDirections client = new MapboxDirections.Builder()
+                .setAccessToken(ACCESS_TOKEN)
+                .setProfile(profile)
+                .setOrigin(coordinates.get(0))
+                .setDestination(coordinates.get(coordinates.size()-1))
+                .setCoordinates(coordinates)
+                .setOverview("full")
+                .build();
+
+
+        client.enqueueCall(new Callback<DirectionsResponse>() {
+            @Override
+            public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                if (response.body() == null || response.body().getRoutes().size() == 0) {
+                    Log.e("Map", "No routes found");
+                    return;
+                }
+
+                DirectionsRoute route = response.body().getRoutes().get(0);
+                drawRoute(route);
+
+            }
+
+            @Override
+            public void onFailure(Call<DirectionsResponse> call, Throwable t) {
+                Log.e("Map", "Error: " + t.getMessage());
+            }
+        });
+    }
+
+    public void drawRoute(DirectionsRoute route) {
+
+        LineString lineString = LineString.fromPolyline(route.getGeometry(), PRECISION_6);
+        List<Position> coordinates = lineString.getCoordinates();
+        LatLng[] points = new LatLng[coordinates.size()];
+        for (int i = 0; i < coordinates.size(); i++) {
+            points[i] = new LatLng(
+                    coordinates.get(i).getLatitude(),
+                    coordinates.get(i).getLongitude());
+        }
+
+        mapboxMap.addPolyline(new PolylineOptions()
+                .add(points)
+                .color(Color.parseColor("#303F9F"))
+                .width(6));
+    }
+
 }
