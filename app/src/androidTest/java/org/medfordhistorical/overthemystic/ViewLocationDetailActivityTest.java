e894978426caa8ed9885c38637c041122fa1bc7b
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2017-11-06 23:05:26 -0500

    added tests for ViewLocationDetail activity

--- app/src/androidTest/java/org/medfordhistorical/overthemystic/ViewLocationDetailActivityTest.java
+++ app/src/androidTest/java/org/medfordhistorical/overthemystic/ViewLocationDetailActivityTest.java
@@ -1,5 +1,6 @@
 package org.medfordhistorical.overthemystic;
 
+import android.content.Intent;
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.ViewInteraction;
@@ -10,80 +11,52 @@ import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
 
 import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MapActivityTest {
+public class ViewLocationDetailActivityTest {
 
     @Rule
-    public ActivityTestRule<MapActivity> mActivityTestRule = new ActivityTestRule<>(MapActivity.class);
+    public ActivityTestRule<ViewLocationDetail> mActivityTestRule = new ActivityTestRule<ViewLocationDetail>(ViewLocationDetail.class) {
+
+
+        @Override
+        protected Intent getActivityIntent() {
+            Intent intent = new Intent();
+            intent.putExtra("siteId", 1);
+            intent.putExtra("siteName", "Ballou Hall");
+            intent.putExtra("siteDesc", "Test text");
+            intent.putExtra("imageUrl", "http://174.138.43.181/directus/storage/uploads/00000000003.jpg");
+            intent.putExtra("audioUrl", "http://174.138.43.181/directus/storage/uploads/00000000004.mp3");
+            return intent;
+        }
+    };
 
     @Test
-    public void clickSiteNavigationFabButton_showsFabMenu() throws Exception{
+    public void descriptionText_isVisibleWihCorrectText() throws Exception{
 
-        ViewInteraction sitesRecyclerView = onView(withId(R.id.rvSite));
-
-        sitesRecyclerView.perform(
-                RecyclerViewActions.actionOnItemAtPosition(0, clickChildViewWithId(R.id.navigate)));
-    }
-
-    @Test
-    public void clickSiteNavigationFabButtonBike_opensNavigationUI() throws Exception {
-
-        ViewInteraction sitesRecyclerView = onView(withId(R.id.rvSite));
-
-        sitesRecyclerView.perform(
-                RecyclerViewActions.actionOnItemAtPosition(0, clickChildViewWithId(R.id.navigate_bike)));
-
-        sitesRecyclerView.check(doesNotExist());
-    }
-
-    @Test
-    public void closeNavigationView_showsSiteDetailView() throws Exception {
-        ViewInteraction sitesRecyclerView = onView(withId(R.id.rvSite));
-
-        sitesRecyclerView.perform(
-                RecyclerViewActions.actionOnItemAtPosition(0, clickChildViewWithId(R.id.navigate_bike)));
-
-        pressBack();
-
-        onView(withId(R.id.locationDescription)).check(matches(isDisplayed()));
+        onView(withId(R.id.locationDescription)).check(matches(isDisplayed())).check(matches(withText("Test text")));
     }
 
     @Test
-    public void finishNavigation_removesSiteFromSiteList() throws Exception {
+    public void clickAudioFabButton_changesFabIcon() throws Exception {
+        ViewInteraction audioFab = onView(withId(R.id.play));
 
+        audioFab.perform(click());
+
+        audioFab.perform(click());
     }
-
-
-    public static ViewAction clickChildViewWithId(final int id) {
-        return new ViewAction() {
-            @Override
-            public Matcher<View> getConstraints() {
-                return null;
-            }
-
-            @Override
-            public String getDescription() {
-                return "Click on a child view with specified id.";
-            }
-
-            @Override
-            public void perform(UiController uiController, View view) {
-                View v = view.findViewById(id);
-                v.performClick();
-            }
-        };
-    }
-
 }
